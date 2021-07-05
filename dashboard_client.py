51c2a5f4ca201377ce76541e460ae1ff0b2c2451
irab21 <65721379+irab21@users.noreply.github.com>
2021-07-05 13:35:04 +0530

    Update dashboard_client.py

--- dashboard_client.py
+++ dashboard_client.py
@@ -10,13 +10,7 @@ from plotly.subplots import make_subplots
 
 
 
-temp_file = st.file_uploader("Enter file here!")
-if temp_file: 
-	temp_file_contents = temp_file.read()
 
-if st.button("Save as working file"):
-    with open("ON_DISK_FILE.extension","wb") as file_handle:
-        file_handle.write(temp_file_contents)
 
 
 image_url='posterityfinal.png'
